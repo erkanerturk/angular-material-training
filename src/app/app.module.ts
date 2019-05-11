a3cc6c45cd054f2dc066fac61293f5ab25b179e5
Erkan Ertürk <erkan.erturk@bulutmd.com>
2019-05-11 23:21:48 +0300

    Typography

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { TypographyComponent } from './typography/typography.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TypographyComponent,
   ],
   imports: [
     BrowserModule,
