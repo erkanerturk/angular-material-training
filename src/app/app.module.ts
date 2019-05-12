84318ef4950da69623f21c30fbf000b6904b87b7
Erkan Ertürk <erkan.erturk@bulutmd.com>
2019-05-12 11:55:14 +0300

    Button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { TypographyComponent } from './typography/typography.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TypographyComponent,
+    ButtonComponent,
   ],
   imports: [
     BrowserModule,
